8a3df739b9d8b37547d9f12c3797dc234cda2e59
OA Wu <comdan66@gmail.com>
2015-11-30 16:55:59 +0800

    Temp

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -8,6 +8,10 @@
 class Main extends Site_controller {
 
   public function index () {
-    $this->load_view ();
+    $pic = Picture::create (array (
+            'name' => 'xxx',
+        ));
+    $pic->name->put_url ('http://images.all-free-download.com/images/graphiclarge/daisy_pollen_flower_220533.jpg');
+    echo $pic->name->url ();;
   }
 }
