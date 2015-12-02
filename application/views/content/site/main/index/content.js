c832cf51076219418182ba8b13e53fd0b3502dd0
OA Wu <comdan66@gmail.com>
2015-12-02 17:56:46 +0800

    Fix index page.

--- application/views/content/site/main/index/content.js
+++ application/views/content/site/main/index/content.js
@@ -10,11 +10,23 @@ $(function () {
     });
   }).resize ();
 
-  $('.p').each (function () {
-    var that = $(this).get (0);
-    that.viewer = new ThetaViewer (that, null, null, $(this).data ('color'));
 
-    // that.viewer.setEnable (false);
+  if ($('#content_url').val ().length) {
+    $.fancybox ({
+        href: $('#content_url').val () + '?t=' + new Date ().getTime (),
+        type: 'iframe',
+        padding: 0,
+        margin: 30,
+        width: '100%',
+        maxWidth: '1200',
+    });
+  }
+
+  $('.b').each (function () {
+    var that = $(this).get (0);
+    var position = $(this).data ('position');
+    that.viewer = new ThetaViewer (that, null, $(this).data ('position'), $(this).data ('color'));
+    that.viewer.setEnable (false);
     that.viewer.images = [$(this).data ('url')];
     that.viewer.load ();
   });
--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -1,8 +1,16 @@
 <div class='ps'>
   <?php
-  foreach (Picture::all () as $pic) { ?>
-    <div class='p' data-url='<?php echo $pic->name->url ('1024w');?>' data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
+  foreach ($pics as $pic) { ?>
+    <div class='p'>
+      <div class='b' data-position='<?php echo json_encode ($pic->position ());?>' data-url='<?php echo $pic->name->url ('1024w');?>' data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
+      <a href='<?php echo base_url ($pic->id);?>'></a>
+      <?php if (Session::getData ('user') === 'oa') { ?>
+        <a class='icon-pencil2 edit'></a>
+        <a class='icon-bin delete'></a>
+      <?php } ?>
+      <a class='icon-share2 share'></a>
+      <a class='icon-location location'></a>
+    </div>
   <?php
   } ?>
-  
 </div>
