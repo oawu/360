a79cda5c5a2c3da34a0fba9d16ec0648868fb23e
OA Wu <comdan66@gmail.com>
2015-12-03 00:28:56 +0800

    Remove demo page.

--- application/views/content/site/main/x/content.js
+++
@@ -1,16 +0,0 @@
-/**
- * @author      OA Wu <comdan66@gmail.com>
- * @copyright   Copyright (c) 2015 OA Wu Design
- */
-
-$(function () {
-  $('.theta-viewer').each (function () {
-    var that = $(this).get (0);
-    that.viewer = new ThetaViewer (that, function (position) {
-console.error (position);
-
-    }, {x: -169.83280551862157, y: 324.91120752739204, z: 128.6111321289731}, $(this).data ('color'));
-    that.viewer.images = [$(this).data ('url')];
-    that.viewer.load ();
-  });
-});
--- application/views/content/site/main/x/content.php
+++
@@ -1,1 +0,0 @@
-<div class='theta-viewer' data-url='<?php echo $pic->name->url ('2048w');?>' data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
