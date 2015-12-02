2e7137d864a306a3e3ef6606476e43a1caa603e3
OA Wu <comdan66@gmail.com>
2015-12-02 17:56:55 +0800

    Add routes.

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,8 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 Route::root ('main');
+Route::get ('/(:id)', 'main@index($1)');
+Route::get ('content/(:id)', 'main@content($1)');
 Route::get ('login', 'main@login');
 Route::post ('login', 'main@signin');
 Route::delete ('logout', 'main@logout');
