00425365927d6aab7cb1433330fbdbc3b2bf9bcc
OA Wu <comdan66@gmail.com>
2015-12-03 17:20:39 +0800

    Fix code

--- resource/javascript/thetaview/theta-viewer.js
+++ resource/javascript/thetaview/theta-viewer.js
@@ -117,7 +117,6 @@
       mapping = new THREE.UVMapping;
       return async.map(this.images, function(img, done) {
         var texture;
-        THREE.ImageUtils.crossOrigin = 'pic.360.ioa.tw';
         return texture = THREE.ImageUtils.loadTexture(img, mapping, function() {
           var material;
           material = new THREE.MeshBasicMaterial({
