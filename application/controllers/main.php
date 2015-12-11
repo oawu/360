6d823b07a8e962c41b41cec65e0f5c0968b677f4
OA Wu <comdan66@gmail.com>
2015-12-11 12:15:47 +0800

    Fix content page.

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -7,32 +7,35 @@
 
 class Main extends Site_controller {
 
-  public function index ($token = 0) {
-    $pics = Picture::find ('all', array (
-        'order' => 'id DESC',
-        'limit' => 20,
-        'conditions' => Session::getData ('user') === 'oa' ? array () : array ('is_visibled = ?', 1)
-      ));
+  public function content ($token = 0) {
+    if (!($pic = Picture::find_by_token ($token)))
+      return redirect_message (array (''), array (
+          '_flash_message' => '當案不存在<br/>或者您的權限不夠喔！'
+        ));
 
-    $pic = Picture::find_by_token ($token, array ('select' => 'id, token, cover, made_at'));
-
-    if ($pic && !preg_match ('/^data:/', $og_img = $pic->cover->url ('1200x630c'))) {
+    if (!preg_match ('/^data:/', $og_img = $pic->cover->url ('1200x630c')))
       $this->add_meta (array ('property' => 'og:image', 'content' => $og_img, 'alt' => $pic->made_at->format ('Y-m-d H:i:s') . ' - ' . Cfg::setting ('site', 'main', 'title')))
            ->add_meta (array ('property' => 'og:image:type', 'content' => 'image/' . pathinfo ($og_img, PATHINFO_EXTENSION)))
            ->add_meta (array ('property' => 'og:image:width', 'content' => '1200'))
            ->add_meta (array ('property' => 'og:image:height', 'content' => '630'));
-    }
 
-    return $this->add_hidden (array ('id' => 'content_url', 'value' => $pic ? base_url ('content', $pic->token) : ''))
-                ->add_css (base_url ('resource', 'css', 'fancyBox_v2.1.5', 'jquery.fancybox.css'))
-                ->add_css (base_url ('resource', 'css', 'fancyBox_v2.1.5', 'jquery.fancybox-buttons.css'))
-                ->add_css (base_url ('resource', 'css', 'fancyBox_v2.1.5', 'jquery.fancybox-thumbs.css'))
-                ->add_css (base_url ('resource', 'css', 'fancyBox_v2.1.5', 'my.css'))
-                ->add_js (base_url ('resource', 'javascript', 'fancyBox_v2.1.5', 'jquery.fancybox.js'))
-                ->add_js (base_url ('resource', 'javascript', 'fancyBox_v2.1.5', 'jquery.fancybox-buttons.js'))
-                ->add_js (base_url ('resource', 'javascript', 'fancyBox_v2.1.5', 'jquery.fancybox-thumbs.js'))
-                ->add_js (base_url ('resource', 'javascript', 'fancyBox_v2.1.5', 'jquery.fancybox-media.js'))
-                ->add_js (base_url ('resource', 'javascript', 'thetaview', 'async.js'))
+    return $this->add_js (base_url ('resource', 'javascript', 'thetaview', 'async.js'))
+                ->add_js (base_url ('resource', 'javascript', 'thetaview', 'three.js'))
+                ->add_js (base_url ('resource', 'javascript', 'thetaview', 'OrbitControls.js'))
+                ->add_js (base_url ('resource', 'javascript', 'thetaview', 'theta-viewer.js'))
+                ->load_view (array (
+                    'pic' => $pic
+                  ));
+  }
+
+  public function index () {
+    $pics = Picture::find ('all', array (
+        'order' => 'id DESC',
+        'limit' => 20,
+        'conditions' => array ('is_visibled = ?', 1)
+      ));
+
+    return $this->add_js (base_url ('resource', 'javascript', 'thetaview', 'async.js'))
                 ->add_js (base_url ('resource', 'javascript', 'thetaview', 'three.js'))
                 ->add_js (base_url ('resource', 'javascript', 'thetaview', 'OrbitControls.js'))
                 ->add_js (base_url ('resource', 'javascript', 'thetaview', 'theta-viewer.js'))
@@ -81,7 +84,7 @@ class Main extends Site_controller {
                       'pic' => $pic
                     ), false);
   }
-  public function content ($token = 0) {
+  public function contendt ($token = 0) {
     $this->set_frame_path ('frame', 'pure');
 
     if (!($pic = Picture::find_by_token ($token)))
--- application/views/content/site/main/content/content.php
+++ application/views/content/site/main/content/content.php
@@ -1,15 +1,19 @@
 <?php
   if (isset ($pic)) { ?>
     <div id='ball'
-         data-cover='<?php echo $pic->cover;?>'
-         data-cover_url='<?php echo base_url ('cover', $pic->token);?>'
-         data-position='<?php echo json_encode ($pic->position ());?>'
-         data-url='<?php echo $pic->name->url ('4096w');?>'
-         data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
+        data-cover='<?php echo $pic->cover;?>'
+        data-cover_url='<?php echo base_url ('cover', $pic->token);?>'
+        data-position='<?php echo json_encode ($pic->position ());?>'
+        data-url='<?php echo $pic->name->url ('4096w');?>'
+        data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'>
+    </div>
+    <a href='' id='prev' class='icon-chevron-left' title='上一張'></a>
+    <a href='' id='next' class='icon-chevron-right' title='下一張'></a>
+    <div id='move' class='icon-move'>試著用拖拉變更視角</div>
 <?php
   } else { ?>
-    <div class='error'>
-      <div>當案不存在，或者您的權限不夠喔！</div>
+    <div id='warning' class='icon-warning'>
+      當案不存在<br/>或者您的權限不夠喔！
     </div>
 <?php
   }
