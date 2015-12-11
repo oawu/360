d0ef1f546abc36a2d2d25cb2547a7db638c80d96
OA Wu <comdan66@gmail.com>
2015-12-11 16:15:13 +0800

    Fix index page layout.

--- application/views/content/site/main/index/content.js
+++ application/views/content/site/main/index/content.js
@@ -4,99 +4,11 @@
  */
 
 $(function () {
-  $(window).resize (function () {
-    $('.p').height ($.makeArray ($('.p').map (function () {
-      return $(this).width ();
-    })).max ());
-  }).resize ();
+  var $linkPanel = $('#link_panel');
+  var $url = $linkPanel.find ('.url').click (function () { $(this).select (); });
+  var $copyMsg = $linkPanel.find ('.m').removeClass ('s');
 
-  $('.link').click (function () {
-    $('#link_panel').addClass ('s');
-  });
-  $('#link_panel .d').click (function () {
-    $('#link_panel').removeClass ('s');
-  });
-  $('#link_panel .c').click (function () {
-    $('#link_panel').removeClass ('s');
-  });
-  var $url = $('.url').click (function () {
-    $(this).select ();
-  });
-
-  $('#link_panel .copy').click (function () {
-      window.getSelection ().removeAllRanges ();
-      var range = document.createRange ();
-      range.selectNode ($url.get (0));
-      window.getSelection ().addRange (range);
-
-      try {
-        var successful = document.execCommand ('copy');
-        $('#link_panel .m').text ('已經複製囉！').addClass ('s');
-      } catch (err) {
-        $('#link_panel .m').text ('GG.. 複製失敗..').addClass ('s');
-      }
-
-      window.getSelection ().removeAllRanges ();
-  });
-
-  $('.location').click (function () {
-    $.fancybox ({
-        href: $(this).data ('url'),
-        type: 'iframe',
-        padding: 0,
-        margin: 30,
-        width: '100%',
-        maxWidth: '1200',
-    });
-  });
-
-  $('.edit').click (function () {
-    $.fancybox ({
-        href: $(this).data ('url'),
-        type: 'iframe',
-        padding: 0,
-        margin: 30,
-        width: '100%',
-        maxWidth: '1200',
-        afterClose: function () {
-          location.reload ();
-        }
-    });
-  });
-
-  $('.share').click (function () {
-    window.open ('https://www.facebook.com/sharer/sharer.php?u=' + $(this).data ('url'), '分享', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
-  });
-
-  $('.delete').click (function () {
-    if (confirm ('確定刪除？'))
-      $.ajax ({
-        url: $(this).data ('url'),
-        data: { },
-        async: true, cache: false, dataType: 'json', type: 'delete',
-      })
-      .done (function (result) {
-        if (result.status)
-          location.reload ();
-        else
-          alert (result.message);
-      })
-      .fail (function (result) { ajaxError (result); })
-      .complete (function (result) { });
-  });
-
-  if ($('#content_url').val ().length) {
-    $.fancybox ({
-        href: $('#content_url').val () + '?t=' + new Date ().getTime (),
-        type: 'iframe',
-        padding: 0,
-        margin: 30,
-        width: '100%',
-        maxWidth: '1200',
-    });
-  }
-
-  $('.b').each (function () {
+  $('.obj').each (function () {
     var $this = $(this);
     var that = $this.get (0);
     var position = $(this).data ('position');
@@ -111,5 +23,114 @@ $(function () {
         uploadCover ($this.data ('cover_url'), $this.find ('canvas').get (0).toDataURL ());
     });
   });
+
+  $('.icon-location').click (function () {
+    console.error ($(this).data ('url'));
+    
+    $.fancybox ({
+        href: $(this).data ('url'),
+        type: 'iframe',
+        padding: 0,
+        margin: 30,
+        width: '100%',
+        maxWidth: '1200',
+    });
+  });
+
+  $('.icon-mail-forward').click (function () {
+    window.open ('https://www.facebook.com/sharer/sharer.php?u=' + $(this).data ('url'), '分享', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
+  });
+  $('.icon-link').click (function () {
+    $linkPanel.addClass ('s');
+    $url.val ($(this).data ('url'));
+    $copyMsg.text ('已經複製囉！').removeClass ('s');
+  });
+  $linkPanel.on ('click', '.d, .c', function () {
+    $linkPanel.removeClass ('s');
+    $url.val ('');
+    $copyMsg.text ('已經複製囉！').removeClass ('s');
+  });
+
+  $linkPanel.on ('click', '.copy', function () {
+    window.getSelection ().removeAllRanges ();
+    var range = document.createRange ();
+    range.selectNode ($url.get (0));
+    window.getSelection ().addRange (range);
+
+    try {
+      if (document.execCommand ('copy'))
+        $copyMsg.text ('已經複製囉！').addClass ('s');
+      else throw 'GG.. 複製失敗..';
+    } catch (err) {
+      $copyMsg.text (err).addClass ('s');
+    }
+    window.getSelection ().removeAllRanges ();
+  });
+
+
+
+
+
+
+  
+  // $(window).resize (function () {
+  //   $('.p').height ($.makeArray ($('.p').map (function () {
+  //     return $(this).width ();
+  //   })).max ());
+  // }).resize ();
+
+
+
+  // $('.edit').click (function () {
+  //   $.fancybox ({
+  //       href: $(this).data ('url'),
+  //       type: 'iframe',
+  //       padding: 0,
+  //       margin: 30,
+  //       width: '100%',
+  //       maxWidth: '1200',
+  //       afterClose: function () {
+  //         location.reload ();
+  //       }
+  //   });
+  // });
+
+  // $('.share').click (function () {
+  //   window.open ('https://www.facebook.com/sharer/sharer.php?u=' + $(this).data ('url'), '分享', 'scrollbars=yes,resizable=yes,toolbar=no,location=yes,width=550,height=420,top=100,left=' + (window.screen ? Math.round(screen.width / 2 - 275) : 100));
+  // });
+
+  // $('.delete').click (function () {
+  //   if (confirm ('確定刪除？'))
+  //     $.ajax ({
+  //       url: $(this).data ('url'),
+  //       data: { },
+  //       async: true, cache: false, dataType: 'json', type: 'delete',
+  //     })
+  //     .done (function (result) {
+  //       if (result.status)
+  //         location.reload ();
+  //       else
+  //         alert (result.message);
+  //     })
+  //     .fail (function (result) { ajaxError (result); })
+  //     .complete (function (result) { });
+  // });
+
+
+  // $('.b').each (function () {
+  //   var $this = $(this);
+  //   var that = $this.get (0);
+  //   var position = $(this).data ('position');
+  //   that.viewer = new ThetaViewer (that, null, $(this).data ('position'), $(this).data ('color'), false, {
+  //     max: 100,
+  //     min: 388
+  //   });
+  //   that.viewer.setEnable (false);
+  //   that.viewer.images = [$(this).data ('url')];
+  //   that.viewer.load (function () {
+  //     if (!($this.data ('cover') && $this.data ('cover').length))
+  //       uploadCover ($this.data ('cover_url'), $this.find ('canvas').get (0).toDataURL ());
+  //   });
+  // });
 
 });
--- application/views/content/site/main/index/content.php
+++ application/views/content/site/main/index/content.php
@@ -1,28 +1,28 @@
-<div class='ps'>
-  <?php
+<div class='balls'>
+<?php
   foreach ($pics as $pic) { ?>
-    <div class='p'>
-<?php if ((string)$pic->cover) { ?>
-        <img class='i_c' src='<?php echo $pic->cover->url ();?>' />
-<?php } else { ?>
-        <div class='b' data-cover='<?php echo $pic->cover;?>' data-cover_url='<?php echo base_url ('cover', $pic->token);?>' data-position='<?php echo json_encode ($pic->position ());?>' data-url='<?php echo $pic->name->url ('1024w');?>' data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
+    <div class='ball'>
+<?php if (!(string)$pic->cover) { ?>
+        <div class='obj'
+             data-cover='<?php echo $pic->cover;?>' 
+             data-cover_url='<?php echo base_url ('cover', $pic->token);?>' 
+             data-position='<?php echo json_encode ($pic->position ());?>' 
+             data-url='<?php echo $pic->name->url ('1024w');?>' 
+             data-color='<?php echo str_replace ('#', '', $pic->color ('hex'));?>'></div>
+        <a href='http://dev.360.ioa.tw/e24fce3f67d948efe1335d5bfc26a840' class='border'></a>
+<?php } else {?>
+        <a href='http://dev.360.ioa.tw/e24fce3f67d948efe1335d5bfc26a840' class='border i_c'>
+          <img class='cover' src="<?php echo $pic->cover->url ();?>" />
+        </a>
 <?php }?>
-      <a href='<?php echo base_url ($pic->token);?>'></a>
-  <?php if (Session::getData ('user') === 'oa') { ?>
-          <a title='編輯檢視角度' class='icon-pencil2 edit l' data-url='<?php echo base_url ('edit', $pic->token);?>'></a>
-          <a title='刪除' class='icon-bin delete l' data-url='<?php echo base_url ($pic->token);?>'></a>
-    <?php if ($pic->is_visibled) { ?>
-            <a title='目前公開' class='icon-eye l' href='<?php echo base_url ('eye', $pic->token);?>' data-method='put'></a>
-    <?php } else { ?>
-            <a title='目前非公開' class='icon-eye-blocked l' href='<?php echo base_url ('eye', $pic->token);?>' data-method='put'></a>
-    <?php }?>
-  <?php } ?>
-      <a title='取得鏈結網址' class='icon-link link' data-url='<?php echo base_url ('link', $pic->token);?>'></a>
-      <a title='分享至臉書' class='icon-share2 share' data-url='<?php echo base_url ($pic->token);?>'></a>
-      <a title='檢視地圖位置' class='icon-location location' data-url='<?php echo base_url ('location', $pic->token);?>'></a>
+      <div class='btns'>
+        <a title='取得鏈結網址' class='l icon-link' data-url='<?php echo base_url ('link', $pic->token);?>'></a>
+        <a title='檢視地圖位置' class='c icon-location' data-url='<?php echo base_url ('location', $pic->token);?>'></a>
+        <a title='分享至臉書' class='r icon-mail-forward' data-url='<?php echo base_url ($pic->token);?>'></a>
+      </div>
     </div>
-  <?php
-  } ?>
+<?php 
+  }?>
 </div>
 
 <div id='link_panel'>
@@ -30,11 +30,11 @@
   <div class='pl'>
     <div class='l'>
       <div>
-        <input type='text' class='url' value='<?php echo base_url ($pic->token);?>' />
+        <input type='text' class='url' value='' />
         <button class='copy'>複製</button>
       </div>
       <div class='m'></div>
     </div>
     <div class='icon-x d'></div>
   </div>
-</div>
+</div> 
