6bfc2bd464ab9a1582a62b594e713f60a731d649
OA Wu <comdan66@gmail.com>
2016-02-12 00:46:06 +0800

    Fix code

--- application/views/content/site/main/location/content.php
+++ application/views/content/site/main/location/content.php
@@ -2,7 +2,7 @@
   if (isset ($pic)) { ?>
     <div class='map'>
       <i></i><i></i><i></i><i></i>
-      <div id='map' data-cover='<?php echo $pic->cover->url ('600x600c');?>' data-lat='<?php echo $pic->latitude;?>' data-lng='<?php echo $pic->longitude;?>'></div>
+      <div id='map' data-cover='<?php echo $pic->cover->url ('640x640c');?>' data-lat='<?php echo $pic->latitude;?>' data-lng='<?php echo $pic->longitude;?>'></div>
     </div>
 <?php
   } else { ?>
